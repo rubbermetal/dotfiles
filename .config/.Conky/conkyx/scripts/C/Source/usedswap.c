65b18ee737e8d34f9936799c00984003a685a6b8
Clayton D Grace <128340307+rubbermetal@users.noreply.github.com>
2026-04-03 03:42:25 +0100

    fix: usedswap.c - fix bugs and safety issues

--- .config/.Conky/conkyx/scripts/C/Source/usedswap.c
+++ .config/.Conky/conkyx/scripts/C/Source/usedswap.c
@@ -1,34 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdint.h>
-#include <inttypes.h>
 #include <sys/sysinfo.h>
-#include <math.h>
-
-const char* format_bytes(uint64_t size) {
-    static const char* size_name[] = {"B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"};
-
-    if (size == 0) {
-        return "0B";
-    }
-
-    int i = floor(log(size) / log(1024));
-    double p = pow(1024, i);
-    double s = round(size / p * 100) / 100;
-
-    static char formatted_str[20]; // Sufficiently large buffer to hold the formatted string
-    snprintf(formatted_str, sizeof(formatted_str), "%.2f %s", s, size_name[i]);
-    return formatted_str;
-}
 
 int main() {
     struct sysinfo info;
     if (sysinfo(&info) == 0) {
-        uint64_t used_swap = info.totalswap - info.freeswap;
+        unsigned long long used_swap =
+            ((unsigned long long)info.totalswap - info.freeswap) * info.mem_unit;
 
-        printf("%s\n", format_bytes(used_swap));
+        if (used_swap == 0) {
+            printf("0 B\n");
+        } else if (used_swap > 1024ULL * 1024ULL * 1024ULL) {
+            printf("%.2f GB\n", (double)used_swap / (1024.0 * 1024.0 * 1024.0));
+        } else if (used_swap > 1024ULL * 1024ULL) {
+            printf("%.2f MB\n", (double)used_swap / (1024.0 * 1024.0));
+        } else if (used_swap > 1024ULL) {
+            printf("%.2f KB\n", (double)used_swap / 1024.0);
+        } else {
+            printf("%llu B\n", used_swap);
+        }
     } else {
-        printf("Error retrieving used swap memory information.\n");
+        printf("Error retrieving swap information.\n");
     }
 
     return 0;
