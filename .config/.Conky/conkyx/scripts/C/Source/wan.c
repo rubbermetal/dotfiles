44b264727f47d1a98c5e942590903bb67ff578a4
Clayton D Grace <128340307+rubbermetal@users.noreply.github.com>
2026-04-03 03:42:24 +0100

    fix: wan.c - fix bugs and safety issues

--- .config/.Conky/conkyx/scripts/C/Source/wan.c
+++ .config/.Conky/conkyx/scripts/C/Source/wan.c
@@ -1,65 +1,73 @@
 #include <stdio.h>
-#include <string.h> // Added for string functions
+#include <stdlib.h>
+#include <string.h>
 #include <curl/curl.h>
 
-// Callback function to write the received data from the HTTP request
-size_t write_callback(char *ptr, size_t size, size_t nmemb, char *data) {
-    // Concatenate the received data to the data buffer
-    strcat(data, ptr);
-    return size * nmemb;
+struct response {
+    char *data;
+    size_t size;
+};
+
+size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
+    struct response *resp = (struct response *)userdata;
+    size_t total = size * nmemb;
+
+    char *tmp = realloc(resp->data, resp->size + total + 1);
+    if (!tmp) {
+        return 0;
+    }
+
+    resp->data = tmp;
+    memcpy(resp->data + resp->size, ptr, total);
+    resp->size += total;
+    resp->data[resp->size] = '\0';
+
+    return total;
 }
 
-// Function to get the WAN IP address of the host
 void get_wan_ip(char *wan_ip) {
     CURL *curl;
     CURLcode res;
-    char data[100] = {0}; // Buffer to store the received data
+    struct response resp = {NULL, 0};
 
-    // Initialize libcurl
     curl = curl_easy_init();
     if (!curl) {
         fprintf(stderr, "Error initializing libcurl\n");
         return;
     }
 
-    // Set the URL to fetch the WAN IP address
     curl_easy_setopt(curl, CURLOPT_URL, "http://checkip.dyndns.org");
-
-    // Set the write callback function to handle the received data
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &resp);
+    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10L);
 
-    // Pass the buffer to store the received data to the write callback function
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, data);
-
-    // Perform the HTTP request
     res = curl_easy_perform(curl);
     if (res != CURLE_OK) {
-        fprintf(stderr, "Error fetching the WAN IP address: %s\n", curl_easy_strerror(res));
+        fprintf(stderr, "Error fetching WAN IP: %s\n", curl_easy_strerror(res));
         curl_easy_cleanup(curl);
+        free(resp.data);
         return;
     }
 
-    // Extract the IP address from the received data
-    char *ip_start = strchr(data, ':');
-    if (ip_start) {
-        char *ip_end = strchr(ip_start, '<');
-        if (ip_end) {
-            *ip_end = '\0'; // Null-terminate the IP address
-            strcpy(wan_ip, ip_start + 2); // Skip ': ' characters
+    if (resp.data) {
+        char *ip_start = strchr(resp.data, ':');
+        if (ip_start) {
+            char *ip_end = strchr(ip_start, '<');
+            if (ip_end) {
+                *ip_end = '\0';
+                strcpy(wan_ip, ip_start + 2);
+            }
         }
     }
 
-    // Clean up libcurl
     curl_easy_cleanup(curl);
+    free(resp.data);
 }
 
 int main() {
     char wan_ip[50] = {0};
 
-    // Call the get_wan_ip() function to retrieve the WAN IP address
     get_wan_ip(wan_ip);
-
-    // Print the WAN IP address
     printf("%s\n", wan_ip);
 
     return 0;
